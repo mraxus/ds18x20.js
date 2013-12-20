6e8661861f760e99fe25160bf11cecf89be54740
Marcus Bergman <marcus.bergman@netlight.com>
2013-12-20 23:48:05 +0100

    Fixed tests for device-reader-read-test. However, more tests need to be added for better coverage.

--- test/integration/device-reader-read-test.js
+++ test/integration/device-reader-read-test.js
@@ -5,14 +5,14 @@ var test = require('tap').test,
 	path = require('path'),
 	DeviceReader = require('../../lib/device-reader');
 
-function readTester(t, testDirName, deviceName, expectedToReturnData) {
+function readTester(t, testDirName, deviceName, expectedOutput) {
 
 	var deviceReader = new DeviceReader(fs, path.resolve(__dirname, 'dirs', testDirName));
 
 	t.test('... synchronous...', function (t) {
 
 		var result = deviceReader.read(deviceName);
-		t.equal(!!result, expectedToReturnData, 'should check if data is returned');
+		t.equal(result, expectedOutput, 'should return expected temperature');
 		t.end();
 	});
 	t.test('... asynchronous...', function (t) {
@@ -20,22 +20,25 @@ function readTester(t, testDirName, deviceName, expectedToReturnData) {
 		deviceReader.read(deviceName, function (err, result) {
 
 			t.notOk(err, 'should not error');
-			t.equal(!!result, expectedToReturnData, 'should check if data is returned');
+			t.equal(result, expectedOutput, 'should return expected temperature');
 			t.end();
 		});
 	});
 	t.end();
 }
 
-test('Getting no device names from directory...', function (t) {
-	readTester(t, 'one-device', '28-011111111111', true);
+//test('Getting no device names from directory...', function (t) {
+//	readTester(t, '0-devices', '28-011111111111', true);
+//});
+test('Getting temperature from one existing device...', function (t) {
+	readTester(t, '1-device', '28-011111111111', 24.312);
 });
-//test('Getting one device name from directory...', function (t) {
-//	readTester(t, 'one-device', ['28-011111111111']);
-//});
-//test('Getting two device names from directory...', function (t) {
-//	readTester(t, 'two-devices', ['28-011111111111', '28-022222222222']);
-//});
+test('Getting temperature from another existing device...', function (t) {
+	readTester(t, '2-devices', '28-011111111111', 24.562);
+});
+test('Getting temperature from yet another existing device...', function (t) {
+	readTester(t, '2-devices', '28-022222222222', 24.562);
+});
 //test('Getting five device names from directory...', function (t) {
 //	readTester(t, 'five-devices', ['28-011111111111', '28-022222222222', '28-033333333333', '28-044444444444', '28-055555555555']);
 //});
