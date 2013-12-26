2a890bf38774d98f764c8080e8ac7d2f7991709d
Marcus Bergman <mraxus@gmail.com>
2013-12-26 23:43:57 +0100

    test/integration/reader-read-test: Updated test to function with what reader.read() does. Also gave all mocked input 'w1_slave' files the same file content for simpler testing.

--- test/integration/reader-read-test.js
+++ test/integration/reader-read-test.js
@@ -3,42 +3,43 @@
 var test = require('tap').test,
 	fs = require('fs'),
 	path = require('path'),
-	DeviceReader = require('../../lib/reader');
+	DeviceReader = require('../../lib/reader'),
 
-function readTester(t, testDirName, deviceName, expectedOutput) {
+    expectedOutput = ["40 06 4b 46 7f ff 10 10 ae : crc=ae YES",
+                        "40 06 4b 46 7f ff 10 10 ae t=100000", ""];
+
+function readTester(t, testDirName, deviceName) {
 
 	var deviceReader = new DeviceReader(fs, path.resolve(__dirname, 'dirs', testDirName));
 
 	t.test('... synchronous...', function (t) {
 
-		var result = deviceReader.read(deviceName);
-		t.equal(result, expectedOutput, 'should return expected temperature');
+		var result = deviceReader.read(deviceName),
+            resultArray = result.split('\n').map(function (str) { return str.trim(); })
+
+		t.deepEqual(resultArray, expectedOutput, 'should return expected temperature');
 		t.end();
 	});
 	t.test('... asynchronous...', function (t) {
 
 		deviceReader.read(deviceName, function (err, result) {
 
+            var resultArray = result.split('\n').map(function (str) { return str.trim(); })
+
 			t.notOk(err, 'should not error');
-			t.equal(result, expectedOutput, 'should return expected temperature');
+			t.deepEqual(resultArray, expectedOutput, 'should return expected temperature');
 			t.end();
 		});
 	});
 	t.end();
 }
 
-//test('Getting no device names from directory...', function (t) {
-//	readTester(t, '0-devices', '28-011111111111', true);
-//});
 test('Getting temperature from one existing device...', function (t) {
-	readTester(t, '1-device', '28-011111111111', 24.312);
+	readTester(t, '1-device', '28-011111111111');
 });
 test('Getting temperature from another existing device...', function (t) {
-	readTester(t, '2-devices', '28-011111111111', 24.562);
+	readTester(t, '2-devices', '28-011111111111');
 });
 test('Getting temperature from yet another existing device...', function (t) {
-	readTester(t, '2-devices', '28-022222222222', 24.562);
+	readTester(t, '2-devices', '28-022222222222');
 });
-//test('Getting five device names from directory...', function (t) {
-//	readTester(t, 'five-devices', ['28-011111111111', '28-022222222222', '28-033333333333', '28-044444444444', '28-055555555555']);
-//});
