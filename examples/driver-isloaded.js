579b11f76e0205ce4e92555a5ddd58728eb37b11
Marcus Bergman <mraxus@gmail.com>
2013-12-27 00:54:47 +0000

    examples/driver-isloaded: Added example file for lib/driver. Should be apart of lib/ds18x20 and not used separately.

---
+++ examples/driver-isloaded.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var Driver = require('../lib/driver'),
+    exec = require('child_process').exec,
+    execSync = require('execSync').run,
+    fs = require('fs'),
+
+    driver = new Driver(fs, exec, execSync, '/sys/bus/w1'),
+    c = console.log,
+    isLoaded;
+
+isLoaded = driver.isLoaded();
+c('isLoaded sync', isLoaded);
+
+driver.isLoaded(function (err, results) { c('isLoaded async', results); });
+
+if (1 || !isLoaded) {
+    
+    driver.load(function (err) {
+        c('load async');
+        if (err) { c(err); c('try running with sudo...'); }
+        else { c('load async successful.'); }
+    });
+
+    c('load sync', driver.load());
+}
