755ed0a66b32c1b15effec26c4ae203e5a64832d
Marcus Bergman <mraxus@gmail.com>
2013-12-29 22:51:33 +0100

    examples/sample-app-sync: Added the rest of the sample to the app. Woohoo.

--- examples/sample-app-sync.js
+++ examples/sample-app-sync.js
@@ -1,24 +1,25 @@
 'use strict';
 
-var driver = require('../index'),
+var sensor = require('../index'),
     out = console.log;
 
 out('Welcome to the sample app for sync handling of the ds18x20 driver.\n');
 
 out('First off, let\'s see if the ds18x20 driver is loaded.');
-out('This we do by calling "driver.isDriverLoaded()":');
+out('This we do by calling "sensor.isDriverLoaded()":');
 out('...');
 
-var isLoaded = driver.isDriverLoaded();
+var isLoaded = sensor.isDriverLoaded();
 
 if (!isLoaded) {
 
     out('The driver is not loaded.');
     out('This means we want to load the driver to be able to continue.');
     out('To do so, we need to be super-user root... Let\'s try!');
+    out('"sensor.loadDriver()":');
 
     try {
-        driver.loadDriver();
+        sensor.loadDriver();
     } catch (err) {
         out('Seems like we are not running this script as a super-user...');
         out('Sorry to say, but we will shut down since we cannot continue with the show.');
@@ -29,18 +30,17 @@ if (!isLoaded) {
         process.exit(0);
     }
 
-    out('Alright. The driver is loaded. Then let\'s continue. Maybe you have some sensors connected...?');
+    out('Alright. The driver is now loaded. Then let\'s continue. Maybe you have some sensors connected...?');
 
 } else {
 
     out('Alright, the driver was already loaded. Let\'s see if you have some sensors hooked up.');
-
 }
 
-out('So, first we want to list all sensors you have: "driver.list()":');
+out('So, first we want to list all sensors you have: "sensor.list()":');
 out('...');
 
-var listOfDeviceIds = driver.list();
+var listOfDeviceIds = sensor.list();
 
 if (listOfDeviceIds.length === 0) {
 
@@ -48,19 +48,53 @@ if (listOfDeviceIds.length === 0) {
     out('Please do so you can continue with this awesome sample app.');
     out('Bye bye for now =-)');
     process.exit(0);
-
 }
 
 out('Woohoo, you seem to have some sensors we can play with =-D.');
 out('These devices were found:', listOfDeviceIds, '\n');
 
 out('Not only can you list the devices.');
-out('You can also measure the temperature of all sensors: "driver.getAll()":');
+out('You can also measure the temperature of all sensors: "sensor.getAll()":');
 out('Each sensor that is hooked up to this raspberry pi is now probed in sequence for their temperature.');
 
 out('...');
 
-var result = driver.getAll();
+var result = sensor.getAll();
 
 out('The result:', result);
+out('');
+out('All results are presented in Celsius degrees and are rounded to one decimal place.');
+out('');
+out('');
 
+var oneId = listOfDeviceIds[listOfDeviceIds.length - 1];
+
+out('If you want to check a specific sensor, say:', oneId);
+out('Then you call: "sensor.get(\'' + oneId + '\')"');
+
+out('...');
+
+var oneResult = sensor.get(oneId);
+
+out('The result:', oneResult);
+out('');
+
+var arrayOfIds = [listOfDeviceIds[0], oneId];
+
+out('There is also the possibility to request several measurements in one request:');
+out('Then you call: "sensor.get(' + arrayOfIds.toString() + ')"');
+
+if (listOfDeviceIds.length === 1) {
+    out('...since you only had one sensor connected, it will be probed twice!');
+}
+
+
+out('...');
+
+var arrayOfResults = sensor.get(arrayOfIds);
+
+out('And here is the result:', arrayOfResults);
+out('Note that the result is presented in the same order as the input array');
+out('');
+
+out('And that\'s it. Hope you find this sample app and the sensor library useful!');
